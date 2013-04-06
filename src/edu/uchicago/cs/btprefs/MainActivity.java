c5eb682f7998f7f442781ed893ce964d28717056
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 19:43:35 -0500

    p0

--- src/edu/uchicago/cs/btprefs/MainActivity.java
+++ src/edu/uchicago/cs/btprefs/MainActivity.java
@@ -18,16 +18,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity  {
 
-	//1: declare a reference to the SharedPreferences class
-	SharedPreferences shp;
-	
-	//declare some refs to the UI objects
-	TextView txtPref;
-	Button btnGet;
-	
-	//PrefsMgr pmgManager;
+
 	
 	
 	@Override
@@ -35,16 +28,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		//2: get the object from the factory
-		shp = PreferenceManager.getDefaultSharedPreferences(this);
 		
-		//get inflated views
-		txtPref = (TextView) findViewById(R.id.txtPref);
-		btnGet = (Button) findViewById(R.id.btnGet);
-		
-		btnGet.setOnClickListener(this);
-		
-		// pmgManager = new PrefsMgr(shp);
 	}
 
 	@Override
@@ -54,55 +38,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 	
-	@Override
-	public void onClick(View v) {
-		
-		
-		Editor edtEditor = shp.edit();
-		
-		//clearing all prefs
-		//edtEditor.clear();
-		
-		//setting an individual preference
-		edtEditor.putInt("MY_INT", 20);
-		edtEditor.putString("MY_STRING", "hello");
-		edtEditor.putBoolean("MY_BOOL", true);
-		//etc.
-		
-		//setting a set of preferences
-		Set<String> strVals = new HashSet<String>();
-		strVals.add("baseball");
-		strVals.add("football");
-		strVals.add("basketball");
-		strVals.add("soccer");
-		edtEditor.putStringSet("MY_SET", strVals);
-		
-		//you must call commit to commit the transaction
-		edtEditor.commit();
-		
-		
-		//getting a preference
-		int nMyInt = shp.getInt("MY_INT", -99);
-		String strMyStr = shp.getString("MY_STRING", "absent");
-		boolean bMyBool = shp.getBoolean("MY_BOOL", false);
-		
-
-		//getting all the preferences
-		StringBuilder stb = new StringBuilder();
-		Map<String, ?> map = shp.getAll();
-		for(Map.Entry<String,?> entry : map.entrySet()){
-			 String strKey = entry.getKey();
-			 Object  objValue = entry.getValue();
-			 stb.append(strKey + " : ");
-			 stb.append(objValue.toString() + "\n");
-		}
-		txtPref.setText(stb.toString());
-
-		//txtPref.setText(pmgManager.getAllPrefsAsString());
-		
-		
-		
-	}
+	
 	
 
 
